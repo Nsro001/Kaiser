e055bbc27deba20c2fef9d6e3962eccc5e98ea69
Nsro001 <jonathan.madrid.araneda@gmail.com>
2025-12-02 20:59:28 -0300

    Fix: Forzado de chunks manuales para pdfmake en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
-  build: {
-    sourcemap: false,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
